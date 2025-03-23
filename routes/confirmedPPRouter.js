7b74cfca7b21944df243c1661358c70ce6f69428
DeveloperTariq786 <shaheendeveloper12@gmail.com>
2025-03-23 12:24:52 +0530

    Update2.0

--- routes/confirmedPPRouter.js
+++ routes/confirmedPPRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ConfirmedPPModel = require('../models/confirmedPPModel');
+const ConfirmedPPModel = require('../models/ConfirmedPPModel');
 const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
